b7cef85411cf93c721f3a03af3a5de32595a454a
Medzd <149968919+Medzd@users.noreply.github.com>
2023-11-07 16:55:41 +0100

    Create Index-6.js

---
+++ Index-6.js
@@ -0,0 +1,11 @@
+<script>
+    function showPopup() {
+      document.getElementById('fullscreenPopup').style.display = 'block';
+    }
+
+    function closePopup() {
+      document.getElementById('fullscreenPopup').style.display = 'none';
+    }
+
+    document.getElementById('showFullScreenPopup').addEventListener('click', showPopup);
+  </script>
